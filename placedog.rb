aa083cb3f4a7b0be24bd244c02df2e667193f5a8
Jon <jon@digital-drip.com>
2011-03-01 19:39:13 -0500

    Fix crop error with equal requested dimensions
    Image would resize to lower than the aspect ratio which would
    cause the crop process to fail with a 1x1 pixel

--- placedog.rb
+++ placedog.rb
@@ -14,22 +14,39 @@ end
 
 
 def get_doggie_image( width, height )
-  img = Magick::Image.read('public/dog01.jpg').first
+  img = get_random_image
+  multiplier = 0.0
+
+  puts "req: #{width}x#{height}"
 
 
-  if width < height
+  if width == height
+    if img.columns < img.rows
+      multiplier = width.to_f / img.columns 
+    else
+      multiplier = height.to_f / img.rows
+    end
+  elsif width < height
     multiplier = height.to_f / img.rows 
-
-    img = img.thumbnail( img.columns*multiplier, img.rows*multiplier )
-    img = img.crop( Magick::CenterGravity, width, height )
-
   else
     multiplier = width.to_f / img.columns 
-
-    img = img.thumbnail( img.columns*multiplier, img.rows*multiplier )
-    img = img.crop( Magick::CenterGravity, width, height )
   end
+
+  puts "multiplier is: #{multiplier}"
+  puts "orig: #{img.columns}x#{img.rows}"
+
+
+  img = img.thumbnail( img.columns*multiplier, img.rows*multiplier )
+  puts "thumb: #{img.columns}x#{img.rows}"
+  img = img.crop( Magick::CenterGravity, width, height )
+
+  puts "after: #{img.columns}x#{img.rows}"
 
   return img
 end
 
+def get_random_image
+  all_pics = Dir.entries( 'images/' ).collect{ |f| f if f =~ /\.jpg/ }.compact
+  
+  Magick::Image.read( "images/#{all_pics[rand(all_pics.count)]}" ).first
+end
